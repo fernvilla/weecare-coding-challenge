0c945af28dd85df9edb764377a9319a3ade9f38c
Fernando Villasenor <fvilla18@gmail.com>
2023-07-25 10:20:58 -0700

    Fix footer to bottom of screen

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,17 +2,18 @@ import Favorites from '../favorites/Favorites';
 import Footer from '../footer/Footer';
 import Header from './../header/Header';
 
+import styles from './Layout.module.scss';
+
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="relative">
+    <div className={styles.siteContent}>
       <Header />
       <main>{children}</main>
       <Footer />
-
       <Favorites />
     </div>
   );
